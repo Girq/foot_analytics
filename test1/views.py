b36f0b56f9182fed356d5b6560e8daf695195a5a
Grigor <grig@atom.home>
2019-01-08 23:23:44 +0100

    origin1

--- test1/views.py
+++ test1/views.py
@@ -80,7 +80,8 @@ def foot(request,):
 
     ax.bar(nor2, dc)
 
-    plt.savefig('/home/grig/PycharmProjects/untitled/test1/static/test1/images/pic.png')
+    #plt.savefig('/home/grig/PycharmProjects/untitled/test1/static/test1/images/pic.png')
+    plt.savefig('/home/Grig117/foot_analytics/test1/static/test1/images/pic.png')
 
 
 # Request --> Filter /standings

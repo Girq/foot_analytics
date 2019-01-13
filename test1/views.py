14e28e2aa38a974949918dafc9f40fb7a69d1e39
Grigor <grig@atom.home>
2019-01-13 01:06:07 +0100

    scs

--- test1/views.py
+++ test1/views.py
@@ -68,7 +68,7 @@ def test(request):
     ax.bar(nor2, dc)
 
     #plt.savefig('/home/grig/PycharmProjects/untitled/test1/static/test1/images/pic.png')
-     plt.savefig('/home/Grig117/foot_analytics/test1/static/test1/images/pic.png')
+    plt.savefig('/home/Grig117/foot_analytics/test1/static/test1/images/pic.png')
 
     # Request --> Filter /standings
 
